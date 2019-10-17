9f23470eb4f0bfe62e81dcabac90f2f9f5c85137
Mitchell Fenner <mitchfen@protonmail.com>
2019-10-16 21:32:20 -0400

    making progress

--- quickSetup.sh
+++ quickSetup.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-sudo apt-get update
-
-sudo apt-get upgrade
-
-sudo apt-get install ranger vim qbittorrent vlc geany keepassx psensor feh xrandr macchanger zsh xfce4-terminal openssh neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo -y
-
+sudo apt-get update && sudo apt-get upgrade
+sleep 2
+sudo apt-get install ranger vim qbittorrent vlc geany keepassx psensor feh macchanger zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance -y
+sleep 2
 ranger --copy-config=all
-
-cd /home/*/Downloads git clone https://github.com/mitchfen/configs.git
+cd /home/*/Downloads && git clone https://github.com/mitchfen/configs.git
 cd configs
 mv .conkyrc /home/*
 mv .i3status-small.conf /home/*
@@ -15,5 +12,6 @@ mv .vimrc /home/*
 mv config /home/*/.config/i3
 cd .
 sudo rm -r configs
-
 sudo update-grub
+sudo apt autoremove -y
+apt list --installed
