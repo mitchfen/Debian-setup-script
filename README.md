6c557301cd5c62587e0b35f537e20f0d50dc71ee
Mitchell Fenner <mitchfen@protonmail.com>
2019-10-31 23:35:37 -0400

    updated readme.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Debian-Setup-Script
-Configures a new Debian/mint/Ubuntu installation with all the programs and configuration files I need  
-Inspired by LARBS `https://github.com/lukesmithxyz/larbs`
+ - Configures a new Debian/mint/Ubuntu installation with all the programs and configuration files I need
+ - Inspired by [LARBS](https://github.com/lukesmithxyz/larbs)
--- quickSetup.sh
+++ quickSetup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
-sleep 2
-sudo apt-get install ranger vim qbittorrent vlc geany keepassx psensor feh macchanger zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance macchanger -y
-sleep 2
+
+sudo apt-get install ranger vim qbittorrent vlc geany keepassx psensor feh macchanger zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance macchanger snapd -y
+
 ranger --copy-config=all
+
 cd /home/*/Downloads && git clone https://github.com/mitchfen/configs.git
 cd configs
 mv .conkyrc /home/*
@@ -12,6 +13,7 @@ mv .vimrc /home/*
 mv config /home/*/.config/i3
 cd .
 sudo rm -r configs
+
 sudo update-grub
 sudo apt autoremove -y
 apt list --installed
