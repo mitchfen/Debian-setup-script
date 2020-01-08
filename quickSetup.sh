28042de345b75cf1d5a207291d37b2caf57f6384
Mitchell Fenner <mitchfen@protonmail.com>
2020-01-08 12:21:46 -0500

    Modify programs installed, snap packages now

--- quickSetup.sh
+++ quickSetup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install ranger vim qbittorrent vlc geany keepassx psensor feh macchanger zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance macchanger snapd -y
+sudo apt-get install ranger vim qbittorrent vlc keepassxc psensor feh zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance snapd -y
 
 ranger --copy-config=all
 
-cd /home/*/Downloads && git clone https://github.com/mitchfen/configs.git
+cd ~/Downloads && git clone https://github.com/mitchfen/configs.git
 cd configs
-mv .conkyrc /home/*
-mv .mitchBar.conf /home/*
-mv .vimrc /home/*
-mv config /home/*/.config/i3
+mv .conkyrc ~/
+mv .mitchBar.conf ~/
+mv .vimrc ~/
+mv config ~/.config/i3
 cd .
 sudo rm -r configs
+
+sudo snap install spotify
+sudo snap install code --classic
 
 sudo update-grub
 sudo apt autoremove -y
