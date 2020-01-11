3ef7802974fda8b1b8fe845924665bfb7cf36248
Mitchell Fenner <mitchfen@protonmail.com>
2020-01-10 21:34:24 -0500

    add ffmpeg

--- quickSetup.sh
+++ quickSetup.sh
@@ -2,16 +2,16 @@
 
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install ranger vim qbittorrent vlc keepassxc psensor feh zsh xfce4-terminal neofetch htop cmatrix i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance snapd -y
+sudo apt-get install ranger vim qbittorrent vlc keepassxc psensor feh zsh xfce4-terminal neofetch htop i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance snapd ffmpeg -y
 
 ranger --copy-config=all
 
 cd ~/Downloads && git clone https://github.com/mitchfen/configs.git
 cd configs
-mv .conkyrc ~/
 mv .mitchBar.conf ~/
 mv .vimrc ~/
 mv config ~/.config/i3
+mv .zshrc ~/
 cd .
 sudo rm -r configs
 
