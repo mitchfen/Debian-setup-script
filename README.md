20c02811b2e8e60e46d3b6bc43e9513938abb3dd
Mitchell Fenner <mitchfen@protonmail.com>
2020-01-31 16:53:08 -0500

    include i3-gaps, comments

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Debian-Setup-Script
- - Configures a new Debian/mint/Ubuntu installation with all the programs and configuration files I need
- - Inspired by [LARBS](https://github.com/lukesmithxyz/larbs)
+
+This bash script configures a new Debian installation with all the programs and configuration files I need.  
+
+It clones and compiles [i3-gaps](https://github.com/Airblader/i3) from source since it is not currently available through in the debian apt repository.  
+
+I plan to update the script to detect the distribution and alter the apt-get commands such that it is compatible with Linux Mint and Ubuntu as well 
+
+Inspired by [LARBS](https://github.com/lukesmithxyz/larbs)
--- quickSetup.sh
+++ quickSetup.sh
@@ -1,23 +1,46 @@
 #!/bin/bash
 
+# Update distro
 sudo apt-get update && sudo apt-get upgrade
 
-sudo apt-get install ranger vim qbittorrent vlc keepassxc psensor feh zsh xfce4-terminal neofetch htop i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance snapd ffmpeg -y
+# Install apt packages
+sudo apt-get install ranger vim vlc keepassxc psensor feh zsh xfce4-terminal neofetch htop i3 xfce4-screenshooter breeze-cursor-theme git wget curl nemo build-essential lxappearance snapd qbittorrent ffmpeg -y
 
-ranger --copy-config=all
+# May be redundant but need to ensure these are present for i3-gaps
+sudo apt-get install gcc make dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
 
-cd ~/Downloads && git clone https://github.com/mitchfen/configs.git
-cd configs
-mv .mitchBar.conf ~/
-mv .vimrc ~/
-mv config ~/.config/i3
-mv .zshrc ~/
-cd .
-sudo rm -r configs
-
+# Install snap packages
 sudo snap install spotify
 sudo snap install code --classic
+sudo snap install signal-desktop
 
+# Build i3-gaps from source - not in Debian apt repository
+cd ~/Downloads
+git clone https://www.github.com/Airblader/i3 i3-gaps
+cd i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+cd ~/Downloads
+sudo rm -r i3-gaps
+
+
+# Move config files to correct directories
+ranger --copy-config=all
+cd ~/Downloads && git clone https://github.com/mitchfen/configs.git
+cd configs
+mv i3 ~/.config
+mv .vimrc ~/
+mv .zshrc ~/
+
+# Cleanup and finishing touches
+cd .
+sudo rm -r configs
 sudo update-grub
 sudo apt autoremove -y
-apt list --installed
+
+# Pretty neofetch goodness
+neofetch
